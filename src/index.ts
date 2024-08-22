682c19d7698273c4860a6e792d22016923d116e4
Alister Norris <882627+alnorris@users.noreply.github.com>
2024-08-22 17:33:25 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,76 @@
-type ErrorObj<T = unknown, D = unknown> = { code: T; stack: string | undefined, details?: D }
-type ErrorResult<T = unknown, D = unknown> = [undefined, ErrorObj<T, D>]
-type PassResult<T = unknown> = [undefined, T]
-type OkResult< T = unknown> = [T, undefined]
+class ReturnItError<
+  const Code extends string,
+  const Details extends unknown,
+> extends Error {
+  code: Code
+  details?: Details
 
-export type EitherResult<R = unknown, E = unknown> = ErrorResult<E> | OkResult<R>
+  constructor(code: Code, details?: Details) {
+    super(code) // Call the parent constructor with the message
 
-export const Err = <const T, const D>(code: T, details?: D): ErrorResult<T, D> => {
-  return [
-    undefined,
-    {
-      code,
-      details,
-      stack: new Error().stack,
-    },
-  ]
+    this.code = this.constructor.name as Code
+    this.details = details
+
+    // Capture the stack trace, excluding the constructor call from it
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+
+  toJSON() {
+    return {
+      code: this.code,
+      details: this.details,
+      stack: this.stack,
+    }
+  }
 }
 
-export const PassErr = <const T extends ErrorObj>(result: T): PassResult<T> => {
+type ErrorResult<T extends string = string, D = unknown> = [
+  undefined,
+  ReturnItError<T, D>,
+]
+type PassResult<T = unknown> = [undefined, T]
+type OkResult<T = unknown> = [T, undefined]
+
+export type EitherResult<R = unknown, E extends string = string> =
+  | ErrorResult<E>
+  | OkResult<R>
+
+export const Err = <const Code extends string, const D>(
+  code: Code,
+  details?: D,
+): ErrorResult<Code, D> => {
+  const newError = new ReturnItError(code, details)
+  return [undefined, newError]
+}
+
+export const PassErr = <
+  const Code extends string,
+  const Details extends unknown,
+  const T extends ReturnItError<Code, Details>,
+>(
+  result: T,
+): PassResult<T> => {
   return [undefined, result]
 }
 
-export const Ok = <const T = undefined>(result: T = undefined as T): OkResult<T> => {
+export const Ok = <const T = undefined>(
+  result: T = undefined as T,
+): OkResult<T> => {
   return [result, undefined]
 }
 
-export type Result<R = unknown, E = unknown> = Promise<EitherResult<R, E>>
+export type Result<R = unknown, E extends string = string> = Promise<
+  EitherResult<R, E>
+>
 
 type AwaitedList<T extends readonly unknown[]> = {
   -readonly [P in keyof T]: Awaited<T[P]>
 }
 
-type ExtractCodeUnion<T> = T extends {
-  code: infer C
-  stack: string | undefined
-}
-  ? C
-  : never
+type ExtractCodeUnion<T> = T extends ReturnItError<infer C, unknown> ? C : never
+
 type ExtractSuccess<T> = T extends [infer S, undefined] ? S : never
 
 type AllResult<T extends Promise<EitherResult>[]> = EitherResult<
@@ -64,4 +99,3 @@ export const All = <T extends Promise<EitherResult>[]>(
     }
   })
 }
-
